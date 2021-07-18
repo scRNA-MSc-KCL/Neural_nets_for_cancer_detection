15b9c503856b6ceabae13fa3bb714b3e1c537813
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-18 09:58:13 +0100

    Update MLP_Optimization.py

--- MLP_Optimization.py
+++ MLP_Optimization.py
@@ -22,32 +22,29 @@ import zipfile
 #Unzip files
 with zipfile.ZipFile("Dataset1_interdataset.zip", 'r') as zip_ref:
     zip_ref.extractall()
-    
+
+#read lables
 labels =pd.read_csv("Labels.csv")
 label_encoder = LabelEncoder()
 labels = label_encoder.fit_transform(labels)
-data = sc.read_csv("Combined_10x_CelSeq2_5cl_data.csv")
-length_dset = len(data)
-data.obs["annot_index"] = range(length_dset)
-#add a sequential index that will be used to cut labels at a later date
-sc.pp.normalize_total(data, target_sum=10000)
-sc.pp.log1p(data)
 labels = pd.Series(labels)
-labels = labels.loc[data.obs["annot_index"]]
 num_labs = len(pd.unique(labels))
-print("number of labels", num_labs)
+print("number of labels", num_lab)
+labels = labels.to_numpy()
+labels = to_categorical(labels, num_lab)
 
+#read data
+data = sc.read_csv("Combined_10x_CelSeq2_5cl_data.csv")
+
+#normalize data
+sc.pp.normalize_total(data, target_sum=10000)
+#logarithmize data
+sc.pp.log1p(data)
+
+#select highly variable genes
 sc.pp.highly_variable_genes(data, n_top_genes=1000)
 data = data[:, data.var.highly_variable]
-print("The shape after regressing out the data is {}".format(data.shape))
-sc.tl.pca(data, svd_solver='arpack')
-print("The shape after regressing out the data is {}".format(data.shape))
-labels = labels.to_numpy()
-labels = to_categorical(labels, 5)
-data = data.X
 
+#create training and test sets
 X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
-y_test = y_test.to_numpy()
-y_train = y_train.to_numpy()
-y_train = to_categorical(y_train, 5)
-y_test = to_categorical(y_test, 5)
+
