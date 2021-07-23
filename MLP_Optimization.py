61447866e8381f1aa2bf2ad04b166d3a3b0ee747
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-23 14:02:13 +0100

    Update MLP_Optimization.py

--- MLP_Optimization.py
+++ MLP_Optimization.py
@@ -107,14 +107,15 @@ def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, epo
 
 #define variables
 Nodes = np.arange(10, 2010, 500)
-activation = ["tanh", "relu", "sigmoid", "softplus", "softsign", "selu", "elu"]
-#activation = ["tanh"]
-optimizer = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
-#optimizer = ["Nadam"]
+#activation = ["tanh", "relu", "sigmoid", "softplus", "softsign", "selu", "elu"]
+activation = ["tanh"]
+#optimizer = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
+optimizer = ["Nadam"]
 epoch = [3]
 #epoch = [5]
 
-loss_function = ["categorical_crossentropy", "poisson","kl_divergence"]
+#loss_function = ["categorical_crossentropy", "poisson","kl_divergence"]
+loss_function = ["categorical_crossentropy"]
 #consider using custom learning rate
 #may or may not get used. See impact on above results
 regularizer = ["l1", "l2", "l1_l2"]
