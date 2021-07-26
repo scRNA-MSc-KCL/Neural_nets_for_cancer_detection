6fab56adc4f5523bbb69dddc71e70f43f56edfa4
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-26 19:30:37 +0100

    Update SVM_DS2.py

--- SVM_DS2.py
+++ SVM_DS2.py
@@ -6,10 +6,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import anndata
 
+#read data
 data = sc.read_csv("human_cell_atlas/krasnow_hlca_10x_UMIs.csv") #26485 x 65662
 data = anndata.AnnData.transpose(data)
 labels = pd.read_csv("human_cell_atlas/krasnow_hlca_10x_metadata.csv") #65662 x 21
-
+print("The original shape of the data is {}".format(data))
 
 #labels
 labels = labels["free_annotation"]
@@ -20,9 +21,6 @@ num_lab = len(pd.unique(labels))
 print("number of labels", num_lab)
 labels = labels.to_numpy()
 
-#read data
-data = anndata.AnnData.transpose(data)
-print("The original shape of the data is {}".format(data))
 
 #normalize data
 sc.pp.normalize_total(data, target_sum=10000)
