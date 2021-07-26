1943cf7891b898f86f6276db78c1c0f3d88fa342
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-26 12:46:38 +0100

    Update ds2.py

--- ds2.py
+++ ds2.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import scanpy as sc
+import numpy as np
+import sklearn
+import tensorflow
+import keras
+import csv
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+import anndata
 
 data = sc.read_csv("human_cell_atlas/krasnow_hlca_facs_counts.csv")
 data_10x = sc.read_csv("human_cell_atlas/krasnow_hlca_10x_UMIs.csv")
@@ -7,7 +20,7 @@ labels = pd.read_csv("human_cell_atlas/krasnow_hlca_facs_metadata.csv")
 labels_10x = pd.read_csv("human_cell_atlas/krasnow_hlca_10x_metadata.csv")
 
 
-print(data)
-print(data_10x)
-print(labels)
-print(labels_10x)
+print("facs counts", data)
+print("UMI counts", data_10x)
+print("facs labels", labels)
+print("10x labels", labels_10x)
