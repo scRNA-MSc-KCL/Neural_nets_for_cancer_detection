b267a7bbdca4e79f7b14a9fb7b625ec87d6cc291
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-27 18:44:50 +0100

    Update SVM_DS3.py

--- SVM_DS3.py
+++ SVM_DS3.py
@@ -11,16 +11,16 @@ import shutil
 
 #Unzip files
 
-with gzip.open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz', 'rb') as f_in:
-    with open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt', 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
+#with gzip.open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz', 'rb') as f_in:
+#    with open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt', 'wb') as f_out:
+#        shutil.copyfileobj(f_in, f_out)
     
-#import csv
+import csv
 
-#txt_file = r"mytxt.txt"
-#csv_file = r"mycsv.csv"
+txt_file = r"GSE131907_Lung_Cancer_raw_UMI_matrix.txt"
+csv_file = r"GSE131907_Lung_Cancer_raw_UMI_matrix.csv"
 
-#in_txt = csv.reader(open(txt_file, "rb"), delimiter = '\t')
-#out_csv = csv.writer(open(csv_file, 'wb'))
+in_txt = csv.reader(open(txt_file, "rb"), delimiter = '\t')
+out_csv = csv.writer(open(csv_file, 'wb'))
 
-#out_csv.writerows(in_txt)
+out_csv.writerows(in_txt)
