1beeab029b420ee70f60a2bbaeac609e2f2983fa
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-27 18:55:57 +0100

    Update SVM_DS3.py

--- SVM_DS3.py
+++ SVM_DS3.py
@@ -15,12 +15,15 @@ import shutil
 #    with open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt', 'wb') as f_out:
 #        shutil.copyfileobj(f_in, f_out)
     
-import csv
+#import csv
 
-txt_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.txt"
-csv_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.csv"
+#txt_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.txt"
+#csv_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.csv"
 
-in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
-out_csv = csv.writer(open(csv_file, 'w'))
+#in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
+#out_csv = csv.writer(open(csv_file, 'w'))
 
-out_csv.writerows(in_txt)
+#out_csv.writerows(in_txt)
+
+l = sc.read_csv(""GSE131907_Lung_Cancer_raw_UMI_matrix.csv")
+print(l)
