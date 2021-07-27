37ac022e0eaaf4c9911086cc6c0811edd6c3d969
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-27 18:05:10 +0100

    Update SVM_DS3.py

--- SVM_DS3.py
+++ SVM_DS3.py
@@ -9,10 +9,9 @@ import zipfile
 
 
 #Unzip files
-#import gzip
-#with gzip.open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz', 'rb') as f:
-#    read_file = pd.read_csv(f)
-#    read_file.to_csv('GSE131907_Lung_Cancer_raw_UMI_matrix.csv')
+import gzip
+with gzip.open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz', 'rb') as f:
+    read_file.to_csv(f)
 
     
 l = sc.read_csv("GSE131907_Lung_Cancer_raw_UMI_matrix.csv")
