8a23a11a7a40534e6d8d8e596ae06bddbed858fa
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-28 12:15:55 +0100

    Update MLP_Optimization.py

--- MLP_Optimization.py
+++ MLP_Optimization.py
@@ -107,7 +107,7 @@ def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, epo
 
 #define variables
 #Nodes = np.arange(500, 500, 0)
-Nodes = 500
+Nodes = [500]
 activation = ["tanh", "relu", "sigmoid", "softplus", "softsign", "selu", "elu"]
 #activation = ["tanh"]
 optimizer = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
