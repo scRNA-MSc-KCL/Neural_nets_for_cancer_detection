fae3094b20513c89d84c9fb05a67cf11bb29398b
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-28 12:06:47 +0100

    Update SVM_DS3.py

--- SVM_DS3.py
+++ SVM_DS3.py
@@ -9,21 +9,19 @@ import zipfile
 import gzip
 import shutil
 
+import os
+filename = 'GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz'
+os.system('gunzip ' + filename)
 #Unzip files
 
-#with gzip.open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz', 'rb') as f_in:
-#    with open('GSE131907_Lung_Cancer_raw_UMI_matrix.txt', 'wb') as f_out:
-#        shutil.copyfileobj(f_in, f_out)
-    
-#import csv
+import csv
 
-#txt_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.txt"
-#csv_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.csv"
+txt_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.txt"
+csv_file = "GSE131907_Lung_Cancer_raw_UMI_matrix.csv"
 
-#in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
-#out_csv = csv.writer(open(csv_file, 'w'))
-
-#out_csv.writerows(in_txt)
+in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
+out_csv = csv.writer(open(csv_file, 'w'))
+out_csv.writerows(in_txt)
 
 l = sc.read_csv("GSE131907_Lung_Cancer_raw_UMI_matrix.csv")
 print(l)
