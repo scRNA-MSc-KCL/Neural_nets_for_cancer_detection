6c0c80778242cc1affb6a1557ce182e98ac6517e
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-02 22:12:23 +0100

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -27,16 +27,14 @@ if args.path == 2:
   labels = labels["free_annotation"]
 if args.path == 3:
   #Unzip files - for dataset 3
-  filename = 'GSE131907_Lung_Cancer_normalized_log2TPM_matrix.txt.gz'
-  os.system('gunzip ' + filename)
+  #filename = 'GSE131907_Lung_Cancer_normalized_log2TPM_matrix.txt.gz'
+  #os.system('gunzip ' + filename)
   #Unzip files
-  txt_file = "GSE131907_Lung_Cancer_normalized_log2TPM_matrix.txt"
-  csv_file = "GSE131907_Lung_Cancer_normalized_log2TPM_matrix.csv"
-  in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
-  out_csv = csv.writer(open(csv_file, 'w'))
-  out_csv.writerows(in_txt)
-  l = sc.read_csv("GSE131907_Lung_Cancer_normalized_log2TPM_matrix.csv")
-  print(l)
+  #txt_file = "GSE131907_Lung_Cancer_normalized_log2TPM_matrix.txt"
+  #csv_file = "GSE131907_Lung_Cancer_normalized_log2TPM_matrix.csv"
+  #in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
+  #out_csv = csv.writer(open(csv_file, 'w'))
+  #out_csv.writerows(in_txt)
   labels =pd.read_csv("GSE131907_Lung_Cancer_cell_annotation.txt", sep = "\t")
   data = sc.read_csv("GSE131907_Lung_Cancer_normalized_log2TPM_matrix.csv")
   labels = labels["Cell_type"]
