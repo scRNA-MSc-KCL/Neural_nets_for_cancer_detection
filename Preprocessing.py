d627f683962a4f631d6b523a96e713e1c1bee183
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-02 19:17:08 +0100

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -31,20 +31,20 @@ if args.path == 3:
   labels =pd.read_csv("GSE131907_Lung_Cancer_cell_annotation.txt", sep = "\t")
   data = sc.read_csv("GSE131907_Lung_Cancer_raw_UMI_matrix.csv")
 
-
-counter = 0
-
 #Unzip files - for dataset 1
 #with zipfile.ZipFile("Dataset1_interdataset.zip", 'r') as zip_ref:
 #    zip_ref.extractall()
 
-label_encoder = LabelEncoder()
-labels = label_encoder.fit_transform(labels)
-labels = pd.Series(labels)
-num_lab = len(pd.unique(labels))
-print("number of labels", num_lab)
-labels = labels.to_numpy()
+def label_adaption(labels):
+  label_encoder = LabelEncoder()
+  labels = label_encoder.fit_transform(labels)
+  labels = pd.Series(labels)
+  num_lab = len(pd.unique(labels))
+  print("number of labels", num_lab)
+  labels = labels.to_numpy()
+  return labels
 
+labels = label_adaption(labels)
 
 #read data
 print("The original shape of the data is {}".format(data))
@@ -58,7 +58,7 @@ sc.pp.log1p(data)
 sc.pp.highly_variable_genes(data, n_top_genes=1000)
 data = data[:, data.var.highly_variable]
 
-print("The original shape of the data is {}".format(data.shape))
+print("The final shape of the data is {}".format(data.shape))
 #sc.tl.pca(data, svd_solver='arpack')
 #print("The shape after performing pca is {}".format(data.shape))
 
@@ -69,5 +69,6 @@ print("The original shape of the data is {}".format(data.shape))
 
 
 data.write("adata_obj_{}".format(args.path))
+np.savetxt("labels_{}.csv".format(args.path), labels, delimiter=",")
 labels.to_csv("labels_{}.csv".format(args.path))
 
