696621044e005d0a4081618eddfaef35490ec7c4
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-07 17:41:32 +0100

    Update CNN_Optimization.py

--- CNN_Optimization.py
+++ CNN_Optimization.py
@@ -1,1 +1,41 @@
+from pyDeepInsight import ImageTransformer, LogScaler
+import matplotlib.ticker as ticker
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from keras.layers import Conv2D 
+from keras.layers import MaxPool2D
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers import BatchNormalization
+import numpy as np
+import pandas as pd
+import sklearn
+import keras
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+import requests
+import zipfile
+import csv
+import scanpy as sc
+import argparse
+import anndata
 
+
+parser = argparse.ArgumentParser(description='Select dataset')
+parser.add_argument('path', type = int)
+
+args = parser.parse_args()
+if args.path == 1:
+  labels =pd.read_csv("labels_1.csv", names = ["X"])
+  data = sc.read("results_1_cnn.h5ad")
+if args.path == 2:
+  labels =pd.read_csv("labels_2.csv", names = ["X"])
+  data = sc.read("results_2_cnn.h5ad")
+
+num_lab = len(labels)
+counter = 0
