d886ac1442cea1dc860474f3601078f50c088d3e
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-07 17:56:07 +0100

    Update CNN_Optimization.py

--- CNN_Optimization.py
+++ CNN_Optimization.py
@@ -25,10 +25,9 @@ import scanpy as sc
 import argparse
 import anndata
 
-
+#Load data
 parser = argparse.ArgumentParser(description='Select dataset')
 parser.add_argument('path', type = int)
-
 args = parser.parse_args()
 if args.path == 1:
   labels =pd.read_csv("labels_1.csv", names = ["X"])
@@ -36,6 +35,27 @@ if args.path == 1:
 if args.path == 2:
   labels =pd.read_csv("labels_2.csv", names = ["X"])
   data = sc.read("results_2.h5ad")
-
 num_lab = len(labels)
 counter = 0
+
+#perform pca on data
+print("The original shape of the data is {}".format(data.shape))
+sc.tl.pca(data, svd_solver='arpack')
+
+#split data
+X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.33, random_state=42)
+
+#scale data
+ln = LogScaler()
+X_train_norm = ln.fit_transform(X_train)
+X_test_norm = ln.transform(X_test)
+
+#split data using pca
+#thoughts, tsne versus pca for image extraction
+it = ImageTransformer(feature_extractor='pca', 
+                      pixels=50, random_state=1701, 
+                      n_jobs=-1)
+plt.figure(figsize=(5, 5))
+_ = it.fit(X_train_norm, plot=True)
+
+fig.savefig('CNN_graphs/test_graph')
