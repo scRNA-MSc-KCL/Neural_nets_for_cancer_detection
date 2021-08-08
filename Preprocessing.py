4fafa505eeb8846d68f36bf2c715247f17374d23
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-08 18:20:35 +0100

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -46,10 +46,10 @@ if args.path == 3:
   results = 'results_3.h5ad'
 if args.path == 4:
   #unzip gz files
-  """import tarfile
-  tf = tarfile.open("GSE131508_RAW.tar")
-  tf.extractall()
-  os.system('gunzip ' + filename)"""
+  #import tarfile
+  #tf = tarfile.open("GSE131508_RAW.tar")
+  #tf.extractall()
+  #os.system('gunzip ' + filename)
   #Unzip files
   #cherry positive
   #filename = 'GSM3783354_4T1_CherryPositive_RawCounts.txt.gz'
@@ -61,8 +61,8 @@ if args.path == 4:
   #out_csv.writerows(in_txt)
   data_pos = pd.read_csv("GSM3783354_4T1_CherryPositive_RawCounts.csv")
   #cherry negative
-  filename = 'GSM3783356_4T1_CherryNegative_RawCounts.txt.gz'
-  os.system('gunzip ' + filename)
+  #filename = 'GSM3783356_4T1_CherryNegative_RawCounts.txt.gz'
+  #os.system('gunzip ' + filename)
   #txt_file = "GSM3783356_4T1_CherryNegative_RawCounts.txt"
   #csv_file = "GSM3783356_4T1_CherryNegative_RawCounts.csv"
   #in_txt = csv.reader(open(txt_file, "r"), delimiter = '\t')
