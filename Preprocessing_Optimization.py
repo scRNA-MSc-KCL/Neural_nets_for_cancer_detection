6f3d4e041de47f7d0b8c09126ab339bbc17f5b60
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-09 18:21:55 +0100

    Update Preprocessing_Optimization.py

--- Preprocessing_Optimization.py
+++ Preprocessing_Optimization.py
@@ -26,7 +26,8 @@ def label_adaption(labels):
   print("number of labels", num_lab)
   labels = labels.to_numpy()
   return labels  
-  
+
+start = time.time()
 
 #3 is UMI raw
 #4 is normalized
@@ -39,81 +40,81 @@ if args.path == 1:
   #Unzip files - for dataset 1
 #with zipfile.ZipFile("Dataset1_interdataset.zip", 'r') as zip_ref:
 #    zip_ref.extractall()
-  labels =pd.read_csv("Labels.csv")
-  data = sc.read_csv("Combined_10x_CelSeq2_5cl_data.csv")
-  results = 'results_1.h5ad'
-if args.path == 2:
-  data = sc.read_csv("human_cell_atlas/krasnow_hlca_10x_UMIs.csv") #26485 x 65662
-  data = anndata.AnnData.transpose(data)
-  #labels = pd.read_csv("human_cell_atlas/krasnow_hlca_facs_metadata.csv") #9409 x 141
-  ##data = sc.read_csv("human_cell_atlas/krasnow_hlca_facs_counts.csv")  #58683 x 9409
-  labels = pd.read_csv("human_cell_atlas/krasnow_hlca_10x_metadata.csv") #65662 x 21
-  labels = labels["free_annotation"]
-  results = 'results_2.h5ad'
-if args.path == 3:
-  #Unzip files - for dataset 3
-  unzip_file('GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz')
-  #Unzip files
-  unzip_gz_file("GSE131907_Lung_Cancer_raw_UMI_matrix.txt", "GSE131907_Lung_Cancer_raw_UMI_matrix.csv")
-  labels =pd.read_csv("GSE131907_Lung_Cancer_cell_annotation.txt", sep = "\t")
-  data = sc.read_csv("GSE131907_Lung_Cancer_raw_UMI_matrix.csv") #29634 x 208506
-  data = anndata.AnnData.transpose(data)
-  labels = labels["Cell_type"]
-  results = 'results_3.h5ad'
-if args.path == 4:
-  #unzip gz files
-  #import tarfile
-  #tf = tarfile.open("GSE131508_RAW.tar")
-  #tf.extractall()
-  #Unzip files
-  #cherry positive
-  #unzip_file('GSM3783354_4T1_CherryPositive_RawCounts.txt.gz')
-  #unzip_file('GSM3783356_4T1_CherryNegative_RawCounts.txt.gz')
-  #unzip_gz_file("GSM3783354_4T1_CherryPositive_RawCounts.txt", "GSM3783354_4T1_CherryPositive_RawCounts.csv")
-  #cherry negative
-  #unzip_file('GSM3783356_4T1_CherryNegative_RawCounts.txt.gz')
-  #unzip_gz_file("GSM3783356_4T1_CherryNegative_RawCounts.txt", "GSM3783356_4T1_CherryNegative_RawCounts.csv")
-  data_pos = pd.read_csv("GSM3783354_4T1_CherryPositive_RawCounts.csv")
-  data_neg = pd.read_csv("GSM3783356_4T1_CherryNegative_RawCounts.csv")
-  data_pos = data_pos.drop(["Unnamed: 0"], axis = 1)
-  data_neg = data_neg.drop(["Unnamed: 0"], axis = 1)
-  data_pos = data_pos.set_index("Gene_Symbol")
-  data_neg = data_neg.set_index("Gene_Symbol")
-  data = pd.concat([data_pos, data_neg], axis = 1)
-  data = data.fillna(0)
-  data = sc.AnnData(data)
-  data.var_names_make_unique() 
-  data.obs_names_make_unique()
-  data = anndata.AnnData.transpose(data)
-  results = 'results_4.h5ad'
-  l_pos = len(data_pos.columns)
-  l_neg = len(data_neg.columns)
-  label_pos = ["Cherry Positive"]*l_pos
-  label_neg = ["Cherry Negative"]*l_neg
-  labels = label_pos + label_neg
+  labels =pd.read_csv("Original_data/Labels.csv")
+  data = sc.read_csv("Original_data/Combined_10x_CelSeq2_5cl_data.csv")
+  file_loc = "test_results/DS1/SVM"
 
 labels = label_adaption(labels)
+filter_genes = [1, 5, 10]
+remove_high_counts = [1000, 2500, 5000]
+normalize = ["yes", "no"]
+filter_method = ["highly_variable", "summary_stat"]
+filter_by_highly_variable_genes = [500, 1000, 2000]
+min_mean = 0.125
+max_mean = 3
+mean_disp = 0.5
+regress_data = ["yes", "no"]
+unit_variance = ["yes", "no"]
+FIGS = "y"
+
+if FIGS = "y":
+  fig = sc.pl.highest_expr_genes(data, n_top=20, )
+  fig.savefig('{}/{}/highly_expressed_genes'.format(file_loc, start))
 
 #read data
 print("The original shape of the data1 is {}".format(data))
+
+#filter data 
+sc.pp.filter_genes(data, min_cells=3)
+#remove mitochonrial
+if FIGS = "y":
+  adata.var['mt'] = data.var_names.str.startswith('MT-')
+  sc.pp.calculate_qc_metrics(data, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+  fig = sc.pl.violin(data, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
+             jitter=0.4, multi_panel=True)
+  fig.savefig('{}/{}/mitochonrial_and_violin_plots'.format(file_loc, start))
+  sc.pl.scatter(data, x='total_counts', y='pct_counts_mt')
+  fig.savefig('{}/{}/pct_counts_mt_scatter'.format(file_loc, start))
+  sc.pl.scatter(data, x='total_counts', y='n_genes_by_counts')
+  fig.savefig('{}/{}/n_genes_by_count_scatter'.format(file_loc, start))
+#remove genes with high counts
+data = data[data.obs.n_genes_by_counts < 2500, :]
 #normalize data
 sc.pp.normalize_total(data, target_sum=10000)
 #logarithmize data
 sc.pp.log1p(data)
 
-#select highly variable genes
-sc.pp.highly_variable_genes(data, n_top_genes=1000)
+#select top x highly variable genes
+#sc.pp.highly_variable_genes(data, n_top_genes=1000)
+#data = data[:, data.var.highly_variable]
+
+#select highly variable genes based on summary statistics
+sc.pp.highly_variable_genes(data, min_mean=0.0125, max_mean=3, min_disp=0.5)
+if FIGS = "y":
+  fig = sc.pl.highly_variable_genes(data)
+  fig.savefig('{}/{}/highly_variable_summary_stats'.format(file_loc, start))
 data = data[:, data.var.highly_variable]
 
+#regress out data
+sc.pp.regress_out(data, ['total_counts', 'pct_counts_mt'])
+
+#scale to unit variance
+sc.pp.scale(data, max_value=10)
+
 print("The final shape of the data is {}".format(data.shape))
-#sc.tl.pca(data, svd_solver='arpack')
-#print("The shape after performing pca is {}".format(data.shape))
 
-#sc.pp.neighbors(data, n_neighbors=10, n_pcs=40)
-#print("The shape after doing neightbours things is {}".format(data.shape))
-#sc.tl.leiden(data)
-#print("The shape after doing leiden thing is {}".format(data.shape))
+#create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
 
-data.write(results)
-print("data shape", data)
-np.savetxt("labels_{}.csv".format(args.path), labels, delimiter=",")
+SVM_list = ["linear"]
+
+# build classifier
+                                                    
+for i in SVM_list:
+  Classifier = sklearn.svm.SVC(kernel = i)
+  Classifier.fit(X_train, y_train)
+  print("the classification result with the current settings and a {} kernal is {}".format(i, Classifier.score(X_test, y_test)))
+  
+  
+end = time.time()
+print("The time taken to complete this program was {}".format(end - start))
