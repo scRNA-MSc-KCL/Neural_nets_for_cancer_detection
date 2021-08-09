99e87be0c3062f15fa7376f1466dc1b63274e2b1
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-09 21:43:34 +0100

    Update Preprocessing_Optimization.py

--- Preprocessing_Optimization.py
+++ Preprocessing_Optimization.py
@@ -79,11 +79,6 @@ if FIGS == "y":
              jitter=0.4, multi_panel=True, save = 'mitochonrial_and_violin_plots.png')
   sc.pl.scatter(data, x='total_counts', y='pct_counts_mt', save = 'mitochonrial_and_violin_plots.png')
   sc.pl.scatter(data, x='total_counts', y='n_genes_by_counts', save ='pct_counts_mt_scatter.png')
-#remove genes with high counts
-data.var['mt'] = data.var_names.str.startswith('MT-')
-sc.pp.calculate_qc_metrics(data, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
-data = data[data.obs.n_genes_by_counts < 2500, :]
-print("B", data.shape)
 #normalize data
 sc.pp.normalize_total(data, target_sum=10000)
 print("C", data.shape)
