23733ddd43300fc7ea419b40332fdaec193a3ec7
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-10 12:22:00 +0100

    Update MLP_Optimization_nodes.py

--- MLP_Optimization_nodes.py
+++ MLP_Optimization_nodes.py
@@ -70,36 +70,35 @@ def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, epo
         for n1 in Nodes:
           for a1 in activation:
             net.add(Dense(n1, activation = a1, input_shape = (data.n_vars,)))
-            for n2 in Nodes:
-              for a2 in activation:
-                counter += 1
-                optimizer_list.append(o)
-                loss_function_list.append(l)
-                activation_layer_1_list.append(a1)
-                activation_layer_2_list.append(a2)
-                epoch_list.append(e)
-                node_1_length_list.append(n1)
-                node_2_length_list.append(n2)
-                net.add(Dense(n2, activation = a2))
-                net.add(Dense(num_lab, activation='softmax'))
-                net.compile(loss=l, optimizer=o)
-                history = net.fit(X_train, y_train,
+            for a2 in activation:
+              counter += 1
+              optimizer_list.append(o)
+              loss_function_list.append(l)
+              activation_layer_1_list.append(a1)
+              activation_layer_2_list.append(a2)
+              epoch_list.append(e)
+              node_1_length_list.append(n1)
+              node_2_length_list.append(n1)
+              net.add(Dense(n1, activation = a2))
+              net.add(Dense(num_lab, activation='softmax'))
+              net.compile(loss=l, optimizer=o)
+              history = net.fit(X_train, y_train,
                             validation_data=(X_test, y_test),
                             epochs=e,batch_size=250)
-                outputs = net.predict(X_test)
-                labels_predicted= np.argmax(outputs, axis=1)
-                y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
-                misclassified =  (np.sum(labels_predicted != y_test_decoded)/(len(y_test_decoded)))*100
-                percentage_misclassified.append(misclassified)
-                print("model number", counter)
+              outputs = net.predict(X_test)
+              labels_predicted= np.argmax(outputs, axis=1)
+              y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
+              misclassified =  (np.sum(labels_predicted != y_test_decoded)/(len(y_test_decoded)))*100
+              percentage_misclassified.append(misclassified)
+              print("model number", counter)
   print(percentage_misclassified)
   df = pd.DataFrame(list(zip(optimizer_list, loss_function_list, epoch_list, node_1_length_list, activation_layer_1_list, node_2_length_list, activation_layer_2_list, percentage_misclassified)),
                         columns =['optimizer', 'loss_function', "epochs", "node1_length", "activation_layer1", "node2_length" , "activation_layer2", "perceptage_misclassified"])
   return df
 
 #define variables
-#Nodes = np.arange(50, 2050, 200)
-Nodes = [250, 500]
+Nodes = np.arange(50, 2050, 200)
+#Nodes = [250, 500]
 #activation = ["tanh", "relu", "sigmoid", "softplus", "softsign", "selu", "elu"]
 activation = ["tanh"]
 #optimizer = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
