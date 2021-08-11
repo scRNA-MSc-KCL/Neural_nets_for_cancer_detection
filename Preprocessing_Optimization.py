407c7c979fac8972ba7853b19f4a7be32af7ea69
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-11 11:28:46 +0100

    Update Preprocessing_Optimization.py

--- Preprocessing_Optimization.py
+++ Preprocessing_Optimization.py
@@ -185,10 +185,10 @@ mean_disp = [0.5, .75]
 unit_var= ["yes", "no"]
 
             
-results_dataframe_method_1 = SVM_Optimizer_Method_1(data, labels, filter_genes,  min_mean, max_mean, mean_disp, normalize, unit_var)  
+#results_dataframe_method_1 = SVM_Optimizer_Method_1(data, labels, filter_genes,  min_mean, max_mean, mean_disp, normalize, unit_var)  
 #results_dataframe_method_1.to_csv("{}/{}/Method_1.csv".format(file_loc, start))  
 
-#results_dataframe_method_2 = SVM_Optimizer_Method_2(data, labels, filter_genes, normalize, filter_by_highly_variable_gene, unit_var)  
+results_dataframe_method_2 = SVM_Optimizer_Method_2(data, labels, filter_genes, normalize, filter_by_highly_variable_gene, unit_var)  
 #results_dataframe_method_2.to_csv("{}/{}/Method_2.csv".format(file_loc, start))  
 
 end = time.time()
