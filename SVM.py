1b8be2e20995074774eb1b586bf177ca1937ed58
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-12 10:04:37 +0100

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -23,12 +23,14 @@ if args.path == 2:
 if args.path == 3:
   labels =pd.read_csv("labels_3.csv", names = ["X"])
   data = sc.read("results_3.h5ad")
+  print("the data has been read")
 if args.path == 4:
   labels =pd.read_csv("labels_4.csv", names = ["X"])
   data = sc.read("results_4.h5ad")
                                                     
 #create training and test sets
 X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
+print("The data has been split")
 
 SVM_list = ["linear", "poly", "rbf", "sigmoid"]
 
@@ -37,6 +39,7 @@ SVM_list = ["linear", "poly", "rbf", "sigmoid"]
 for i in SVM_list:
   Classifier = sklearn.svm.SVC(kernel = i)
   Classifier.fit(X_train, y_train)
+  print("classifier {} trained".format(i))
   print("the classification result with the current settings and a {} kernal is {}".format(i, Classifier.score(X_test, y_test)))
   
   
