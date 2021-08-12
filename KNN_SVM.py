495f04fe5de764967575cffdb43586166c5a65a0
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-12 16:54:25 +0100

    Create KNN_SVM.py

--- KNN_SVM.py
+++ KNN_SVM.py
@@ -9,32 +9,32 @@ import time
 import argparse
 import anndata
 from sklearn.neighbors import NearestNeighbors
-from sklearn.neighbors import KNeighborsClassifier
 
-labels =pd.read_csv("labels_1.csv", names = ["X"])
-data = sc.read("results_1.h5ad")
-
-X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
-neigh = KNeighborsClassifier(n_neighbors=1000)
+start = time.time()
+labels =pd.read_csv("labels_3.csv", names = ["X"])
+data = sc.read("results_3.h5ad")
+X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.1)
+neigh = KNeighborsClassifier(n_neighbors=2000)
 neigh.fit(X_train, y_train)
 y_train = y_train.reset_index()
-knn_list = []
-SVM_list = []
-
+mode_list = []
 for i in X_test:
   neighbours = neigh.kneighbors([i], return_distance = False)
   SVM_data = X_train[neighbours]
   indices = neighbours.tolist()
   indices = indices[0]
   SVM_labels = y_train['x'][indices]
-  knn_list.append(SVM_labels.value_counts().idxmax())
+  mode_list.append(SVM_labels.value_counts().idxmax())
   SVM_labels = SVM_labels.to_list()
-  Classifier = sklearn.svm.SVC(kernel = "linear")
-  #print(SVM_data[0])
-  Classifier.fit(SVM_data[0], SVM_labels)
-  y_pred = Classifier.predict([i])
-  SVM_list.append(y_pred)
 
-
-print("The accuracy using knn is {}".format(mode_list))
-print("The accuract using SVM is {}".format(y_pred))
+knn_accuracy = 0
+knnsvm_accuracy = 0
+for i in range(len(y_train)):
+  if y_train[i] == mode_list[i]:
+    knn_accuracy += 1
+  if y_train[i] == SVM_labels[i]:
+    knnsvm_accuracy += 1
+knn_accuracy = (knn_accuracy/len(y_train))*100
+    
+end = time.time()
+print("The time taken to complete this program was {}".format(end - start))
