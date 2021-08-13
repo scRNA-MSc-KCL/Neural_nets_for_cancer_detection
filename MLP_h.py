def4172ce0866470b72c4c9c7eca8a0281a7ad38
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-13 22:20:20 +0100

    Update MLP_h.py

--- MLP_h.py
+++ MLP_h.py
@@ -91,7 +91,7 @@ def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, epo
               net.compile(loss=l, optimizer=o)
               history = net.fit(X_train, y_train,
                             validation_data=(X_test, y_test),
-                            epochs=e,batch_size=25)
+                            epochs=e,batch_size=250)
               outputs = net.predict(X_test)
               labels_predicted= np.argmax(outputs, axis=1)
               y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
