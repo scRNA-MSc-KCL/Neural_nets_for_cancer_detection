cebb703f174379e1d4e2b82e9b333d0ea3d21c93
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-15 13:39:46 +0100

    Update MLP_g.py

--- MLP_g.py
+++ MLP_g.py
@@ -9,7 +9,6 @@ from sklearn.preprocessing import LabelEncoder
 from keras.models import Sequential
 from keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras import initializers
 import requests
 import zipfile
 import csv
@@ -59,21 +58,21 @@ counter = 0
 
 #create training and test sets
 X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
+X_test, X_val, y_test, y_val= train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
 #make labels for neural network catagorical
 y_train = to_categorical(y_train, num_lab)
 y_test = to_categorical(y_test, num_lab)
+y_val = to_categorical(y_val, num_lab)
 
 
 #Neural network testing function
-def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, epoch, Nodes, activation, counter, num_lab, b, layer_number, kernal_init):
+def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, X_val, y_val, epoch, Nodes, activation, counter, num_lab, b, layer_number, kernal_init):
   optimizer_list = []
   loss_function_list = []
   epoch_list = []
   node_1_length_list = []
   activation_layer_1_list = []
-  node_2_length_list = []
-  activation_layer_2_list = []
   accuracy_list = []
   layer_number_list = []
   kernal_initializer_list = []
@@ -82,65 +81,61 @@ def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, epo
       for e in epoch:
         for n1 in Nodes:
           for a1 in activation:
-            for a2 in activation:
-              for lr in layer_number:
-                for k in kernal_init:
-                  net = Sequential()
-                  if lr == 1:
-                    net.add(Dense(n1, kernel_initializer = k, activation = a1, input_shape = (data.n_vars,)))
-                  if lr == 2:
-                    net.add(Dense(n1, kernel_initializer = k, activation = a1, input_shape = (data.n_vars,)))
-                    net.add(Dense(n1, activation = a2, kernel_initializer = k))
-                  if lr == 3:
-                    net.add(Dense(n1, kernel_initializer = k, activation = a1, input_shape = (data.n_vars,)))
-                    net.add(Dense(n1, kernel_initializer = k, activation = a2))
-                    net.add(Dense(n1, kernel_initializer = k, activation = a2))
-                  net.add(Dense(num_lab, activation='softmax', kernel_initializer = k))
-                  counter += 1
-                  layer_number_list.append(lr)
-                  optimizer_list.append(o)
-                  loss_function_list.append(l)
-                  activation_layer_1_list.append(a1)
-                  activation_layer_2_list.append(a2)
-                  epoch_list.append(e)
-                  node_1_length_list.append(n1)
-                  node_2_length_list.append(n1)
-                  kernal_initializer_list.append(k)
-                  net.compile(loss=l, optimizer=o)
-                  history = net.fit(X_train, y_train,
-                                  validation_data=(X_test, y_test),
+            for lr in layer_number:
+              for k in kernal_init:
+                net = Sequential()
+                if lr == 1:
+                  net.add(Dense(n1, activation = a1, kernel_initializer = k, input_shape = (data.n_vars,)))
+                if lr == 2:
+                  net.add(Dense(n1, activation = a1,kernel_initializer = k, input_shape = (data.n_vars,)))
+                  net.add(Dense(n1, activation = a1, kernel_initializer = k))
+                if lr == 3:
+                  net.add(Dense(n1, activation = a1, kernel_initializer = k, input_shape = (data.n_vars,)))
+                  net.add(Dense(n1, activation = a1, kernel_initializer = k))
+                  net.add(Dense(n1, activation = a1, kernel_initializer = k))
+                net.add(Dense(num_lab, activation='softmax'))
+                counter += 1
+                layer_number_list.append(lr)
+                optimizer_list.append(o)
+                loss_function_list.append(l)
+                activation_layer_1_list.append(a1)
+                epoch_list.append(e)
+                node_1_length_list.append(n1)
+                kernal_initializer_list.append(k)
+                net.compile(loss=l, optimizer=o)
+                history = net.fit(X_train, y_train,
+                                  validation_data=(X_val, y_val),
                                   epochs=e,batch_size=b)
-                  outputs = net.predict(X_test)
-                  labels_predicted= np.argmax(outputs, axis=1)
-                  y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
-                  correctly_classified =  (np.sum(labels_predicted == y_test_decoded)/(len(y_test_decoded)))*100
-                  accuracy_list.append(correctly_classified)
-                  print("model number", counter)
-                  print("accuracy", correctly_classified)
-                  fig = plt.figure()
-                  plt.plot(history.history['loss'], label='training loss')
-                  plt.plot(history.history['val_loss'], label='validation loss')
-                  plt.xlabel('epochs')
-                  plt.ylabel('loss')
-                  plt.legend()
-                  fig.savefig('test_results/{}/{}/fig_{}'.format(file_loc, start, counter))
+                outputs = net.predict(X_test)
+                labels_predicted= np.argmax(outputs, axis=1)
+                y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
+                correctly_classified =  (np.sum(labels_predicted == y_test_decoded)/(len(y_test_decoded)))*100
+                accuracy_list.append(correctly_classified)
+                print("model number", counter)
+                print("accuracy", correctly_classified)
+                fig = plt.figure()
+                plt.plot(history.history['loss'], label='training loss')
+                plt.plot(history.history['val_loss'], label='validation loss')
+                plt.xlabel('epochs')
+                plt.ylabel('loss')
+                plt.legend()
+                fig.savefig('test_results/{}/{}/fig_{}'.format(file_loc, start, counter))
   print(accuracy_list)
-  df = pd.DataFrame(list(zip(optimizer_list, loss_function_list, epoch_list, node_1_length_list, activation_layer_1_list, node_2_length_list, activation_layer_2_list, accuracy_list, layer_number_list, kernal_initializer_list)),
-                        columns =['optimizer', 'loss_function', "epochs", "node1_length", "activation_layer1", "node2_length" , "activation_layer2", "perceptage_misclassified", "layer_number", "kernal_initializer"])
+  df = pd.DataFrame(list(zip(optimizer_list, loss_function_list, epoch_list, node_1_length_list, activation_layer_1_list, accuracy_list, layer_number_list, kernal_initializer_list)),
+                          columns =['optimizer', 'loss_function', "epochs", "node1_length", "activation_layer1", "perceptage accuracy", "layer_number", "kernal"])
   return df
 
 #define variables
 #Nodes = np.arange(50, 2050, 500)
 Nodes = [500]
-#Nodes = [50, 100, 250, 500, 550, 1000, 1500, 2050]
+#Nodes = np.arange(10, 3010, 10)
 #activation = ["tanh", "relu", "sigmoid", "softplus", "softsign", "selu", "elu"]
 activation = ["tanh"]
 #optimizer = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
 optimizer = ["Adam"]
 #epoch = [100]
-epoch = [5]
-#layer_number = [1, 2, 3]
-layer_number = [2]
+epoch = [7]
+layer_number = [1,2,3]
 
 #loss_function = ["categorical_crossentropy", "poisson","kl_divergence"]
 loss_function = ["categorical_crossentropy"]
@@ -149,5 +144,6 @@ loss_function = ["categorical_crossentropy"]
 #regularizer = ["l1", "l2", "l1_l2"]
 kernal_init = ["random_normal", "random_uniform", "truncated_normal", "zeros", "ones", "glorot_normal", "glorot_uniform", "he_normal", "he_uniform", "identity", "orthogonal", "variance_scaling"]
 
-results_dataframe = MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, epoch, Nodes, activation, counter, num_lab, b, layer_number, kernal_init)
+results_dataframe = MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, X_val, y_val, epoch, Nodes, activation, counter, num_lab, b, layer_number, kernal_init)
 results_dataframe.to_csv("test_results/{}/{}.csv".format(file_loc, start))
+
