4b29414d10d82bf20063c42c649203c86913ba76
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-15 19:12:26 +0100

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -33,48 +33,25 @@ def label_adaption(labels):
 
 parser = argparse.ArgumentParser(description='Select dataset')
 parser.add_argument('path', type = int)
-
 args = parser.parse_args()
 if args.path == 1:
-  #Unzip files - for dataset 1
-#with zipfile.ZipFile("Dataset1_interdataset.zip", 'r') as zip_ref:
-#    zip_ref.extractall()
-  labels =pd.read_csv("Labels.csv")
-  data = sc.read_csv("Combined_10x_CelSeq2_5cl_data.csv")
-  results = 'results_1.h5ad'
+  labels =pd.read_csv("Original_data/Labels.csv")
+  data = sc.read_csv("Original_data/Combined_10x_CelSeq2_5cl_data.csv")
 if args.path == 2:
-  data = sc.read_csv("human_cell_atlas/krasnow_hlca_10x_UMIs.csv") #26485 x 65662
+  data = sc.read_csv("Original_data/human_cell_atlas/krasnow_hlca_10x_UMIs.csv") #26485 x 65662
   data = anndata.AnnData.transpose(data)
   #labels = pd.read_csv("human_cell_atlas/krasnow_hlca_facs_metadata.csv") #9409 x 141
   ##data = sc.read_csv("human_cell_atlas/krasnow_hlca_facs_counts.csv")  #58683 x 9409
-  labels = pd.read_csv("human_cell_atlas/krasnow_hlca_10x_metadata.csv") #65662 x 21
+  labels = pd.read_csv("Original_data/human_cell_atlas/krasnow_hlca_10x_metadata.csv") #65662 x 21
   labels = labels["free_annotation"]
-  results = 'results_2.h5ad'
 if args.path == 3:
-  #Unzip files - for dataset 3
-  unzip_file('GSE131907_Lung_Cancer_raw_UMI_matrix.txt.gz')
-  #Unzip files
-  unzip_gz_file("GSE131907_Lung_Cancer_raw_UMI_matrix.txt", "GSE131907_Lung_Cancer_raw_UMI_matrix.csv")
-  labels =pd.read_csv("GSE131907_Lung_Cancer_cell_annotation.txt", sep = "\t")
-  data = sc.read_csv("GSE131907_Lung_Cancer_raw_UMI_matrix.csv") #29634 x 208506
+  labels =pd.read_csv("Original_data/GSE131907_Lung_Cancer_cell_annotation.txt", sep = "\t")
+  data = sc.read_csv("Original_data/GSE131907_Lung_Cancer_raw_UMI_matrix.csv") #29634 x 208506
   data = anndata.AnnData.transpose(data)
   labels = labels["Cell_type"]
-  results = 'results_3.h5ad'
 if args.path == 4:
-  #unzip gz files
-  #import tarfile
-  #tf = tarfile.open("GSE131508_RAW.tar")
-  #tf.extractall()
-  #Unzip files
-  #cherry positive
-  #unzip_file('GSM3783354_4T1_CherryPositive_RawCounts.txt.gz')
-  #unzip_file('GSM3783356_4T1_CherryNegative_RawCounts.txt.gz')
-  #unzip_gz_file("GSM3783354_4T1_CherryPositive_RawCounts.txt", "GSM3783354_4T1_CherryPositive_RawCounts.csv")
-  #cherry negative
-  #unzip_file('GSM3783356_4T1_CherryNegative_RawCounts.txt.gz')
-  #unzip_gz_file("GSM3783356_4T1_CherryNegative_RawCounts.txt", "GSM3783356_4T1_CherryNegative_RawCounts.csv")
-  data_pos = pd.read_csv("GSM3783354_4T1_CherryPositive_RawCounts.csv")
-  data_neg = pd.read_csv("GSM3783356_4T1_CherryNegative_RawCounts.csv")
+  data_pos = pd.read_csv("Original_data/GSM3783354_4T1_CherryPositive_RawCounts.csv")
+  data_neg = pd.read_csv("Original_data/GSM3783356_4T1_CherryNegative_RawCounts.csv")
   data_pos = data_pos.drop(["Unnamed: 0"], axis = 1)
   data_neg = data_neg.drop(["Unnamed: 0"], axis = 1)
   data_pos = data_pos.set_index("Gene_Symbol")
@@ -85,12 +62,12 @@ if args.path == 4:
   data.var_names_make_unique() 
   data.obs_names_make_unique()
   data = anndata.AnnData.transpose(data)
-  results = 'results_4.h5ad'
   l_pos = len(data_pos.columns)
   l_neg = len(data_neg.columns)
   label_pos = ["Cherry Positive"]*l_pos
   label_neg = ["Cherry Negative"]*l_neg
   labels = label_pos + label_neg
+
 
 labels = label_adaption(labels)
 
