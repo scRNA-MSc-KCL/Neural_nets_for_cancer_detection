f06ec0f087f76a1ceb013a44e5395b201b9a19a6
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-16 12:40:10 +0100

    Update KNN_SVM.py

--- KNN_SVM.py
+++ KNN_SVM.py
@@ -19,7 +19,7 @@ neighbours = [100, 500, 1000, 2000, 3000]
 
 for n in neighbours:
   X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state = 42)
-  neigh = KNeighborsClassifier(n_neighbors=2000)
+  neigh = KNeighborsClassifier(n_neighbors=n)
   neigh.fit(X_train, y_train)
   y_train = y_train.reset_index()
   y_test = y_test.reset_index()
