f181044ba52da3e079f3dc09638a061d1d41c389
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-17 14:12:36 +0100

    Update CNN_c.py

--- CNN_c.py
+++ CNN_c.py
@@ -91,7 +91,7 @@ for p in pixels:
     it = ImageTransformer(feature_extractor=f, 
                           pixels=p, random_state=1701, 
                           n_jobs=-1)
-   fig = plt.figure(figsize=(5, 5))
+    fig = plt.figure(figsize=(5, 5))
     _ = it.fit(X_train, plot=True)
 
     #fig.savefig('{}/{}/fig_1'.format(file_loc, start, p, f))
