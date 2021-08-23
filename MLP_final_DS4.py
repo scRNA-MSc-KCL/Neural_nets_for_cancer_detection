26ad58ace0c4d5579912e6d89ed1ea85bdf9b96e
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-23 10:40:28 +0100

    Update MLP_final_DS4.py

--- MLP_final_DS4.py
+++ MLP_final_DS4.py
@@ -62,7 +62,7 @@ num_lab = len(labels["X"].unique())
 
 #####KNERARSEST NEIGHBOUR########################
 #Separate training and test set
-X_split, X_test, y_split y_test = train_test_split(data.X, labels, test_size=0.2)
+X_split, X_test, y_split, y_test = train_test_split(data.X, labels, test_size=0.2)
 
 y_split = y_split.reset_index(drop = True)
 y_test = y_test.reset_index(drop = True)
