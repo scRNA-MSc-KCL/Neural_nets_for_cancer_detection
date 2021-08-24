3123afb0913997299719b1e58d35f90e8d96c4c0
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-24 21:30:39 +0100

    Update MLP_Optimization.py

--- MLP_Optimization.py
+++ MLP_Optimization.py
@@ -90,7 +90,7 @@ def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, X_v
                     net.add(Dense(n1, activation = a1, kernel_initializer = k, kernel_regularizer=r, input_shape = (data.n_vars,)))
                   if lr == 2:
                     net.add(Dense(1200, activation = "relu", kernel_initializer = k, kernel_regularizer=r, input_shape = (data.n_vars,)))
-                    net.add(Dense(n1, activation = a1, kernel_initializer = k, kernel_regularizer=r))
+                    net.add(Dense(1300, activation = "relu", kernel_initializer = k, kernel_regularizer=r))
                   net.add(Dense(num_lab, activation='softmax'))
                   counter += 1
                   layer_number_list.append(lr)
@@ -127,15 +127,15 @@ def MLP_Assembly(optimizer, loss_function, X_train, y_train, X_test, y_test, X_v
 #define variables
 #Nodes = np.arange(50, 2050, 500)
 #Nodes = [1200]
-Nodes = np.arange(100, 2000, 100)
+#Nodes = np.arange(100, 2000, 100)
 #activation = ["tanh", "relu", "sigmoid", "softplus", "softsign", "selu", "elu"]
 activation = ["relu"]
 #optimizer = ["RMSprop", "Adam", "Adamax", "Nadam"]
-#optimizer = ["Adam"]
+optimizer = ["Adam"]*100
 #epoch = [100]
 epoch = [7]
-layer_number = [1]
-#layer_number = [2]
+#layer_number = [1]
+layer_number = [2]
 
 #loss_function = ["categorical_crossentropy", "poisson","kl_divergence"]
 loss_function = ["categorical_crossentropy"]
