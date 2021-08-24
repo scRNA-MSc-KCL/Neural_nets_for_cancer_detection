759e8d2a3eb2280532fcb044d2bc7cfa9aed030e
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-24 21:27:27 +0100

    Update MLP_final_DS4.py

--- MLP_final_DS4.py
+++ MLP_final_DS4.py
@@ -97,41 +97,6 @@ for train_index, test_index in kf.split(X_split):
   fig.savefig('test_results/{}/{}/fig_{}'.format(file_loc, start, counter))
   accuracy_list.append(correctly_classified)
 
-#X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
-#X_test, X_val, y_test, y_val= train_test_split(X_test, y_test, test_size=0.5, random_state=42)
-
-#####SKLEARN MODEL#############################
-
-"""for i in range(10):
-  X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2)
-  X_test, X_val, y_test, y_val= train_test_split(X_test, y_test, test_size=0.5)
-  y_train = to_categorical(y_train, num_lab)
-  y_test = to_categorical(y_test, num_lab)
-  y_val = to_categorical(y_val, num_lab)
-  counter += 1
-  net = Sequential()
-  net.add(Dense(1200, activation = "relu", kernel_initializer = "glorot_normal", kernel_regularizer="l1_l2", input_shape = (data.n_vars,)))
-  net.add(Dense(1300, activation = "relu", kernel_initializer = "glorot_normal", kernel_regularizer="l1_l2"))
-  net.add(Dense(num_lab, activation='softmax'))
-  net.compile(loss="categorical_crossentropy", optimizer="Adam")
-  history = net.fit(X_train, y_train, validation_data=(X_val, y_val),epochs=20,batch_size=b)
-  outputs = net.predict(X_test)
-  labels_predicted= np.argmax(outputs, axis=1)
-  print(labels_predicted.shape)
-  y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
-  print(y_test_decoded.shape)
-  correctly_classified =  (np.sum(labels_predicted == y_test_decoded)/(len(y_test_decoded)))*100
-  print("model number", counter)
-  print("accuracy", correctly_classified)
-  fig = plt.figure()
-  plt.plot(history.history['loss'], label='training loss')
-  plt.plot(history.history['val_loss'], label='validation loss')
-  plt.xlabel('epochs')
-  plt.ylabel('loss')
-  plt.legend()
-  fig.savefig('test_results/{}/{}/fig_{}'.format(file_loc, start, counter))
-  accuracy_list.append(correctly_classified)"""
-
 #define variables
 print(accuracy_list)
 df = pd.DataFrame(list(zip(accuracy_list)),columns =['accuracy_list'])
