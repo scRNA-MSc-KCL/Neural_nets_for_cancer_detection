6519495468fa5dda1ab7b61c11228a40459f9224
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-27 19:23:31 +0100

    Update SVM_final.py

--- SVM_final.py
+++ SVM_final.py
@@ -70,7 +70,8 @@ for train_index, test_index in kf.split(data.X):
   print("y_test", y_test.shape)
   Classifier = sklearn.svm.SVC(kernel = "rbf")
   Classifier.fit(X_train, y_train)
-  print(y_test.shape)
+  print("y_test", y_test.shape)
+  print(y_test[:10])
   print("the classification result with the current settings  is {}".format(Classifier.score(X_test, y_test)))
   y_pred = Classifier.predict(X_test)
   test_acc = 0
@@ -78,8 +79,8 @@ for train_index, test_index in kf.split(data.X):
     if y_pred[i] == y_test[i]:
       test_acc += 1
   print("accuracy different way", test_acc/len(y_pred))
-    
-  print(y_pred.shape)
+  print("y_pred", y_pred.shape)
+  print(y_pred[:10])
   with open('test_results/{}/{}/summary{}.txt'.format(file_loc, start, counter), 'w') as fr:
     fr.write("precision score: {}".format(precision_score(y_test, y_pred, average=None)))
     fr.write("recall score: {} ".format(recall_score(y_test, y_pred, average=None)))
