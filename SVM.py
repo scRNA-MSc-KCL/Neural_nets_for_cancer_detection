44988bab2b64c1644ba30111a7253b117ef352df
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-27 12:51:51 +0100

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -31,12 +31,10 @@ if args.path == 4:
   
 num_lab = len(labels["X"].unique())
 label_encoder = LabelEncoder()
-labels = label_encoder.fit_transform(labels)
-print(labels.shape)                                                    
+labels = label_encoder.fit_transform(labels)                                                   
 #create training and test sets
-X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
-print(y_train.shape)
-print(y_test.shape)
+X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2)
+
 
 #SVM_list = ["linear", "poly", "rbf", "sigmoid"]
 SVM_list = ["rbf"]
