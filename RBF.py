6a04a9c029b54e216ca90252dd267ad528bd3cf0
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-29 15:54:24 +0100

    Update RBF.py

--- RBF.py
+++ RBF.py
@@ -30,24 +30,19 @@ if args.path == 1:
   data = sc.read("results_1.h5ad")
   file_loc = "DS1/RBF"
   b = 50
-  e = 200
+  e = 100
 if args.path == 2:
   labels =pd.read_csv("labels_2.csv", names = ["X"])
   data = sc.read("results_2.h5ad")
   file_loc = "DS2/RBF"
   b = 500
   e = 100
-if args.path == 3:
-  labels =pd.read_csv("labels_3.csv", names = ["X"])
-  data = sc.read("results_3.h5ad")
-  file_loc = "DS3/RBF"
-  b = 2000
 if args.path == 4:
   labels =pd.read_csv("labels_4.csv", names = ["X"])
   data = sc.read("results_4.h5ad")
   file_loc = "DS4/RBF"
   b = 50
-  e = 600
+  e = 100
   
 path = os.getcwd()
 path = os.path.join(path, "test_results/{}/{}".format(file_loc,start))
