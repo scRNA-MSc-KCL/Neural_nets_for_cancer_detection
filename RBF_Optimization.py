696f4ff0fc2b72a4dc0d356c2d104ec5540d5758
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-30 20:29:49 +0100

    Update RBF_Optimization.py

--- RBF_Optimization.py
+++ RBF_Optimization.py
@@ -1,3 +1,4 @@
+  
 import numpy as np
 import pandas as pd
 import sklearn
@@ -67,47 +68,45 @@ y_val = to_categorical(y_val, num_lab)
 betas = [.0001, .001, .01, .1, 2]
 inititializer = [InitCentersKMeans(X_train), InitCentersRandom(X_train)]
 optimizer = ["RMSprop", "Adam", "Adamax", "Nadam"]
-activation = ["tanh", "relu", "sigmoid", "softplus", "softsign", "selu", "elu"]
-number_of_models = 100
+
 
 accuracy_list = []
 betas_list = []
 inititializer_list = []
 optimizer_list = []
-activation_list = []
 
-for i in range(number_of_models):
-  net = Sequential()
-  net.add(RBFLayer(num_lab,initializer=InitCentersKMeans(X_train),betas=.0001,input_shape=(data.n_vars,)))
-  net.add(Dense(num_lab, activation='softmax'))
-  net.compile(loss="categorical_crossentropy", optimizer="adam")
-  history = net.fit(X_train, y_train,validation_data=(X_val, y_val),epochs=e,batch_size=b)
-  outputs = net.predict(X_test)
-  labels_predicted= np.argmax(outputs, axis=1)
-  y_test_decoded = np.argmax(y_test, axis=1) 
-  correctly_classified =  (np.sum(labels_predicted == y_test_decoded)/(len(y_test_decoded)))*100
-  print("accuracy", correctly_classified)
-  #fig = plt.figure()
-  #plt.plot(history.history['loss'], label='training loss')
-  #plt.plot(history.history['val_loss'], label='validation loss')
-  #plt.xlabel('epochs')
-  #plt.ylabel('loss')
-  #plt.legend()
-  #fig.savefig('test_results/{}/{}/fig'.format(file_loc, start))
-  net.summary()
-  from contextlib import redirect_stdout
-  with open('test_results/{}/{}/model_summary.txt'.format(file_loc, start), 'w') as f:
-      with redirect_stdout(f):
+for i in inititializer:
+  for be in betas:
+    for o in optimizer:
+      net = Sequential()
+      net.add(RBFLayer(num_lab,initializer=i,betas=be,input_shape=(data.n_vars,)))
+      net.add(Dense(num_lab, activation='softmax'))
+      net.compile(loss="categorical_crossentropy", optimizer=o)
+      history = net.fit(X_train, y_train,validation_data=(X_val, y_val),epochs=e,batch_size=b)
+      outputs = net.predict(X_test)
+      labels_predicted= np.argmax(outputs, axis=1)
+      y_test_decoded = np.argmax(y_test, axis=1) 
+      correctly_classified =  (np.sum(labels_predicted == y_test_decoded)/(len(y_test_decoded)))*100
+      print("accuracy", correctly_classified)
+      net.summary()
+      from contextlib import redirect_stdout
+      with open('test_results/{}/{}/model_summary.txt'.format(file_loc, start), 'w') as f:
+        with redirect_stdout(f):
           net.summary()
-  f = open('test_results/{}/{}/model_summary.txt'.format(file_loc, start), 'a')
-  f.write("percentage accuracy on test set is {}\n".format(correctly_classified))
-  f.write("number of epochs is {}".format(e))
-  print("percentage accuracy; ", correctly_classified)
-  accuracy_list.append(correctly_classified)
+      f = open('test_results/{}/{}/model_summary.txt'.format(file_loc, start), 'a')
+      f.write("percentage accuracy on test set is {}\n".format(correctly_classified))
+      f.write("number of epochs is {}".format(e))
+      print("percentage accuracy; ", correctly_classified)
+      accuracy_list.append(correctly_classified)
+      inititializer_list.append(i)
+      optimizer_list.append(o)
+      betas_list.append(be)
   
 
-df = pd.DataFrame(list(zip(accuracy_list)),
-                          columns =['accuracy'])
+df = pd.DataFrame(list(zip(accuracy_list, betas_list,inititializer_list,optimizer_list)),
+                          columns =['accuracy', 'betas', 'intiailizer','optimizer'])
+#df = pd.DataFrame(list(zip(accuracy_list,inititializer_list)),
+                          columns =['accuracy', 'intiailizer')
 end = time.time()
 print("The time taken to complete this program was {}".format(end - start))
 df.to_csv("test_results/{}/{}.csv".format(file_loc, start))
