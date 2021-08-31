6c0a80fdd0859936f7209074db604747dd92237c
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-08-31 09:21:41 +0100

    Update RBF_d.py

--- RBF_d.py
+++ RBF_d.py
@@ -91,7 +91,7 @@ for i in number_of_models:
   with open('test_results/{}/{}/model_summary.txt'.format(file_loc, start), 'w') as f:
     with redirect_stdout(f):
       net.summary()
-   f = open('test_results/{}/{}/model_summary.txt'.format(file_loc, start), 'a')
+   f = open('test_results/{}/{}/model_summary.txt'.format(file_loc, start), 'a'))
    f.write("percentage accuracy on test set is {}\n".format(correctly_classified))
    f.write("number of epochs is {}".format(e))
    print("percentage accuracy; ", correctly_classified)
