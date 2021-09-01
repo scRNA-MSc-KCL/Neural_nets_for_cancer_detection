5998ad69a758aca277cb2d167153a97e22280830
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-09-01 22:16:05 +0100

    Update MLP_a.py

--- MLP_a.py
+++ MLP_a.py
@@ -133,7 +133,7 @@ activation = ["relu"]
 optimizer = ["Adam"]*100
 #epoch = [100]
 epoch = [7]
-layer_number = [2]
+layer_number = [1]
 
 #loss_function = ["categorical_crossentropy", "poisson","kl_divergence"]
 loss_function = ["categorical_crossentropy"]
