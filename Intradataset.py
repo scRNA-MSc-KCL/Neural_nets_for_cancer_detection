7fa6ffa00b3e77c8c7114203fd4b8f12d4e8d2a1
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-09-01 17:03:10 +0100

    Update Intradataset.py

--- Intradataset.py
+++ Intradataset.py
@@ -10,6 +10,11 @@ import anndata
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
 
 labels_10x =pd.read_csv("labels_5.csv", names = ["X"])
 labels_10x = labels_10x['X']
@@ -29,4 +34,24 @@ print("The intradataset nearest neighbour score;", neigh.score(data_celseq.X, la
 
 Classifier = sklearn.svm.SVC(kernel = 'rbf')
 Classifier.fit(data_10x.X, labels_10x)
-print("The intradataset nearest SVM score {}".format(Classifier.score(data_celseq.X, labels_celseq)))
+print("The intradataset SVM score {}".format(Classifier.score(data_celseq.X, labels_celseq)))
+                                                              
+#Multilayer Perceptron
+X_train, X_val, y_train, y_val = train_test_split(data_10x.X, labels_10x, test_size=0.2, random_state=42)
+#make labels for neural network catagorical
+y_train = to_categorical(y_train, num_lab)
+y_test = to_categorical(labels_celseq, num_lab)
+y_val = to_categorical(y_val, num_lab)
+
+#Build Network
+net = Sequential()
+net.add(Dense(500, activation = 'relu', input_shape = (data_10x.n_vars,)))
+net.add(Dense(5, activation='softmax'))
+net.compile(loss="categorical_crossentropy", optimizer='Adam')
+history = net.fit(X_train, y_train,validation_data=(X_val, y_val),epochs=7,batch_size=50)
+outputs = net.predict(data_celseq.X)
+labels_predicted= np.argmax(outputs, axis=1)
+y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
+correctly_classified =  (np.sum(labels_predicted == y_test_decoded)/(len(y_test_decoded)))*100
+print("The intradataset MLP score {}".format(correctly_classified))
+                                                              
