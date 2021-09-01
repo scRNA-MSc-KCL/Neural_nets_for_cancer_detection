4f75e2a0db53cb6defca361ccd9f743ef5d7ff64
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-09-01 16:31:11 +0100

    Create Intradataset.py

---
+++ Intradataset.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import scanpy as sc
+import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import time
+import argparse
+import anndata
+from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+labels_10x =pd.read_csv("labels_5.csv", names = ["X"])
+data_10x = sc.read("results_5.h5ad")
+labels_celseq =pd.read_csv("labels_6.csv", names = ["X"])
+data_celseq = sc.read("results_6.h5ad")
+
+#K Nearest Neighbours
+
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(data_10x, labels_10x)
+print("The intradataset nearest neighbour score;" neigh.score(labels_celseq, data_celseq))
