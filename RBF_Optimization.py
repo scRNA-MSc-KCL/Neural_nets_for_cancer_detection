9dab03a84a8ff4819c06ee049944a8fc3d5dcdff
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-09-03 10:07:47 +0100

    Update RBF_Optimization.py

--- RBF_Optimization.py
+++ RBF_Optimization.py
@@ -25,19 +25,26 @@ parser = argparse.ArgumentParser(description='Select dataset')
 parser.add_argument('path', type = int)
 start = time.time()
 
+#Load Data
 args = parser.parse_args()
+
+#Dataset 1
 if args.path == 1:
   labels =pd.read_csv("labels_1.csv", names = ["X"])
   data = sc.read("results_1.h5ad")
   file_loc = "DS1/RBF"
   b = 50
   e = 200
+  
+#Dataset 2
 if args.path == 2:
   labels =pd.read_csv("labels_2.csv", names = ["X"])
   data = sc.read("results_2.h5ad")
   file_loc = "DS2/RBF"
   b = 500
   e = 100
+  
+#Dataset 3
 if args.path == 4:
   labels =pd.read_csv("labels_4.csv", names = ["X"])
   data = sc.read("results_4.h5ad")
@@ -54,6 +61,7 @@ except OSError:
 else:
   print("Successfully created the directory %s" % path)
 
+#split data
 X_train, X_test, y_train, y_test = train_test_split(data.X, labels, test_size=0.2, random_state=42)
 X_test, X_val, y_test, y_val= train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
@@ -65,16 +73,16 @@ y_train = to_categorical(y_train, num_lab)
 y_test = to_categorical(y_test, num_lab)
 y_val = to_categorical(y_val, num_lab)
 
+#Define variables
 betas = [.0001, .001, .01, .1, 2]
 inititializer = [InitCentersKMeans(X_train), InitCentersRandom(X_train)]
 optimizer = ["RMSprop", "Adam", "Adamax", "Nadam"]
-
-
 accuracy_list = []
 betas_list = []
 inititializer_list = []
 optimizer_list = []
 
+#Create multiple RBF Models
 for i in inititializer:
   for be in betas:
     for o in optimizer:
@@ -83,6 +91,7 @@ for i in inititializer:
       net.add(Dense(num_lab, activation='softmax'))
       net.compile(loss="categorical_crossentropy", optimizer=o)
       history = net.fit(X_train, y_train,validation_data=(X_val, y_val),epochs=e,batch_size=b)
+      #Test models
       outputs = net.predict(X_test)
       labels_predicted= np.argmax(outputs, axis=1)
       y_test_decoded = np.argmax(y_test, axis=1) 
@@ -102,11 +111,9 @@ for i in inititializer:
       optimizer_list.append(o)
       betas_list.append(be)
   
-
+#Output results
 df = pd.DataFrame(list(zip(accuracy_list, betas_list,inititializer_list,optimizer_list)),
                           columns =['accuracy', 'betas', 'intiailizer','optimizer'])
-#df = pd.DataFrame(list(zip(accuracy_list,inititializer_list)),
-                          columns =['accuracy', 'intiailizer')
 end = time.time()
 print("The time taken to complete this program was {}".format(end - start))
 df.to_csv("test_results/{}/{}.csv".format(file_loc, start))
