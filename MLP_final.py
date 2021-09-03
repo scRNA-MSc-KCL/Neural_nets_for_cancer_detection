552fd384441a32eb8ff10ceaed687bb3eeacb3e3
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-09-03 09:52:17 +0100

    Update MLP_final.py

--- MLP_final.py
+++ MLP_final.py
@@ -29,23 +29,29 @@ parser = argparse.ArgumentParser(description='Select dataset')
 parser.add_argument('path', type = int)
 start = time.time()
 
+#Load Data
 args = parser.parse_args()
+
+#Dataset 1
 if args.path == 1:
   labels =pd.read_csv("labels_1.csv", names = ["X"])
   data = sc.read("results_1.h5ad")
   file_loc = "DS1/MLP/Final"
   b = 50
+#Dataset 2
 if args.path == 2:
   labels =pd.read_csv("labels_2.csv", names = ["X"])
   data = sc.read("results_2.h5ad")
   file_loc = "DS2/MLP/Final"
   b = 500
+#Dataset 3
 if args.path == 4:
   labels =pd.read_csv("labels_4.csv", names = ["X"])
   data = sc.read("results_4.h5ad")
   file_loc = "DS4/MLP/Final"
   b = 50
-  
+
+#Create working directory
 path = os.getcwd()
 path = os.path.join(path, "test_results/{}/{}".format(file_loc,start))
 try:
@@ -70,6 +76,8 @@ for train_index, test_index in kf.split(X_split):
   y_train, y_val = y_split['X'][train_index], y_split['X'][test_index]
   y_train = to_categorical(y_train, num_lab)
   y_val = to_categorical(y_val, num_lab)
+  
+  #Create model
   net = Sequential()
   if args.path == 1:
     net.add(Dense(500, activation = "tanh"))
@@ -80,8 +88,12 @@ for train_index, test_index in kf.split(X_split):
     net.add(Dense(1200, activation = "relu", kernel_initializer = "glorot_normal", kernel_regularizer="l1_l2", input_shape = (data.n_vars,)))
     net.add(Dense(1300, activation = "relu", kernel_initializer = "glorot_normal", kernel_regularizer="l1_l2"))
   net.add(Dense(num_lab, activation='softmax'))
+  
+  #Train model
   net.compile(loss="categorical_crossentropy", optimizer="Adam")
   history = net.fit(X_train, y_train, validation_data=(X_val, y_val),epochs=7,batch_size=b)
+  
+  #Test model
   outputs = net.predict(X_test)
   labels_predicted= np.argmax(outputs, axis=1)
   y_test_decoded = np.argmax(y_test, axis=1)  # maybe change so you're not doing every time
@@ -99,7 +111,7 @@ for train_index, test_index in kf.split(X_split):
   counter +=1
   accuracy_list.append(correctly_classified)
 
-#define variables
+#output results
 print(accuracy_list)
 df = pd.DataFrame(list(zip(accuracy_list)),columns =['accuracy_list'])
 df.to_csv("test_results/{}/{}.csv".format(file_loc, start))
